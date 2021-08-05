2e0f535765ce3e0f47822685db6621540ad82171
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-08-05 08:26:08 +0200

    Update config.cpp

--- Addons/Base/config.cpp
+++ Addons/Base/config.cpp
@@ -1,6 +1,6 @@
 class CfgPatches
 {
-	class 9JgKp-Base
+	class 9JgKp_Base
 	{
 		// Meta information for editor
 		name = "9. JgKp Insignias";
@@ -11,7 +11,7 @@ class CfgPatches
 		requiredVersion = 2.0; 
 		// Required addons, used for setting load order.
 		// When any of the addons is missing, pop-up warning will appear when launching the game.
-		requiredAddons[] = { "A3_Functions_F" };
+		requiredAddons[] = {};
 		// List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
 		units[] = {};
 		// List of weapons (CfgWeapons classes) contained in the addon.
