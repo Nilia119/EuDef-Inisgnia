69cf00f2c776d927ba95d16394d7439c9d29abb0
Nilia119 <88453817+Nilia119@users.noreply.github.com>
2021-08-09 17:27:34 +0200

    Update config.cpp

--- Addons/Base/config.cpp
+++ Addons/Base/config.cpp
@@ -1,17 +1,17 @@
 class CfgPatches
 {
-	class 9JgKp_Base
+	class 9JgKp_Insiginia
 	{
 		// Meta information for editor
-		name = "9. JgKp Insignias";
+		name = "Insignien der 9. Jägerkompanie";
 		author = "Nilia";
 		url = "https://github.com/Nilia119/9JgKp-Inisgnia";
 
 		// Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game. Note: was disabled on purpose some time late into Arma 2: OA.
-		requiredVersion = 2.0; 
+		requiredVersion = 1.60;
 		// Required addons, used for setting load order.
 		// When any of the addons is missing, pop-up warning will appear when launching the game.
-		requiredAddons[] = {};
+		requiredAddons[] = { "A3_Functions_F" };
 		// List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
 		units[] = {};
 		// List of weapons (CfgWeapons classes) contained in the addon.
