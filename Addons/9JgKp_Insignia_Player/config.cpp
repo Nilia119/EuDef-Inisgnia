ff7beb9d79b909b7745af8bfb7a4c5cc499c607c
Nilia119 <niklas.rieck@gmail.com>
2023-02-04 15:25:44 +0100

    Fixed Spacing

--- Addons/9JgKp_Insignia_Player/config.cpp
+++ Addons/9JgKp_Insignia_Player/config.cpp
@@ -22,8 +22,8 @@ class CfgPatches
 class 9JgKp_Insignia_Logo;
 
 /* Beispiel:
-  class 9Jgkp_Insignia_NAME: 9JgKp_Insignia_Logo
 
+  class 9Jgkp_Insignia_NAME: 9JgKp_Insignia_Logo
     {
       displayName = "ANZEIGENAME DES PATCHES";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\SPIELERNAME.paa";
@@ -34,16 +34,13 @@ class 9JgKp_Insignia_Logo;
 class CfgUnitInsignia {
 
   class 9Jgkp_Insignia_Nilia: 9JgKp_Insignia_Logo
-
     {
       displayName = "9. JgKp Nilia";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Nilia.paa";
 	  author="Nilia";
   };
 
-
 	class 9Jgkp_Insignia_McNamMon: 9JgKp_Insignia_Logo
-
 	{
 	  displayName = "McNamaras-Morons";
 	  texture = "\9JgKp_Insignia_Player\Player\Bilder\McNamMon.paa";
@@ -51,7 +48,6 @@ class CfgUnitInsignia {
   };
 
 	class 9Jgkp_Insignia_Fox: 9JgKp_Insignia_Logo
-
 	{
 	  displayName = "9. JgKp Fox";
 	  texture = "\9JgKp_Insignia_Player\Player\Bilder\Fox.paa";
@@ -59,7 +55,6 @@ class CfgUnitInsignia {
   };
 
 	class 9Jgkp_Insignia_Juggernaut: 9JgKp_Insignia_Logo
-
     {
       displayName = "9. JgKp Juggernaut";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Juggernaut.paa";
@@ -67,65 +62,62 @@ class CfgUnitInsignia {
   };
 
 	class 9Jgkp_Insignia_EwL: 9JgKp_Insignia_Logo
-
     {
       displayName = "9. JgKp EwL | Jesus";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\EwL.paa";
 	  author="Nilia";
   };
-	class 9Jgkp_Insignia_Plutokratie: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_Plutokratie: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Plutokratie";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Plutokratie.paa";
 	  author="Nilia";
   };
-	class 9Jgkp_Insignia_Stiffler: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_Stiffler: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Stiffler";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Stiffler.paa";
 	  author="Paul";
   };
-	class 9Jgkp_Insignia_Toyo: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_Toyo: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Toyo";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Toyo.paa";
 	  author="Nilia";
   };
-	class 9Jgkp_Insignia_Farid_Khaleel: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_Farid_Khaleel: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Farid Khaleel";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Farid_Khaleel.paa";
 	  author="Nilia";
   };
-	class 9Jgkp_Insignia_Gerstie: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_Gerstie: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Gerstie";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Gerstie.paa";
 	  author="Paul";
   };
-	class 9Jgkp_Insignia_ImTheKessel: 9JgKp_Insignia_Logo
 
+	class 9Jgkp_Insignia_ImTheKessel: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp ImTheKessel";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\ImTheKessel.paa";
 	  author="Paul";
   };
-  
- 	class 9Jgkp_Insignia_Haasm: 9JgKp_Insignia_Logo
 
+ 	class 9Jgkp_Insignia_Haasm: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Haasm1";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Haasm1.paa";
 	  author="Nilia";
   };
-  
-  class 9Jgkp_Insignia_Hotshrek: 9JgKp_Insignia_Logo
 
+  class 9Jgkp_Insignia_Hotshrek: 9JgKp_Insignia_Logo
     {
       displayName = "9. JgKp Hotshrek";
       texture = "\9JgKp_Insignia_Player\Player\Bilder\Hotshrek.paa";
