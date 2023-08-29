cc0b469f3e21c506fecbbe49bc93c3498b72a124
Nilia119 <niklas.rieck@gmail.com>
2023-08-29 21:16:37 +0200

    Added Paul

--- Addons/9JgKp_Insignia_Player/config.cpp
+++ Addons/9JgKp_Insignia_Player/config.cpp
@@ -149,6 +149,13 @@ class CfgUnitInsignia {
 	  author="Nilia";
   };
 
+    class 9Jgkp_Insignia_Paul: 9JgKp_Insignia_Logo
+    {
+        displayName = "9. JgKp Paul";
+        texture = "\9JgKp_Insignia_Player\Player\Bilder\Paul.paa";
+        author="Nilia";
+    };
+
 
 
 //HIER In der nächsten Zeile einfügen
