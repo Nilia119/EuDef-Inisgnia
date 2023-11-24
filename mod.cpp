737e1ab5f7236b341e31b43edcebcf50e17107ed
Nilia119 <niklas.rieck@gmail.com>
2023-11-24 22:28:08 +0100

    Changed Name and Icon

--- mod.cpp
+++ mod.cpp
@@ -1,18 +1,18 @@
-picture = "9JgKp_Logo.paa";		// logo 512x512
-contentBrowserPicture = "9JgKp_Logo.paa";
-logo = "9JgKp_Logo.paa";  //Logo displayed in bottom left of main menu
-logoOver = "9JgKp_Logo.paa"; //Logo displayed in the mod menu when mod icon is clicked 
-logoSmall = "9JgKp_Logo.paa"; //Logo displayed next to all units and vehicles in the editor 
+picture = "EuDef_Logo.paa";		// logo 512x512
+contentBrowserPicture = "EuDef_Logo.paa";
+logo = "EuDef_Logo.paa";  //Logo displayed in bottom left of main menu
+logoOver = "EuDef_Logo.paa"; //Logo displayed in the mod menu when mod icon is clicked 
+logoSmall = "EuDef_Logo.paa"; //Logo displayed next to all units and vehicles in the editor 
 
 dlcColor[] = {0,0,0,1}; //Something about color
 
 hideName = 0; //Hide the name of the mod
 hidePicture = 0; //Hide the logo of the mod
-tooltip = "9. Jägerkompanie Insignien";
-tooltipOwned = "9. Jägerkompanie Insignien"; //Tooltip when mod icon is moused over in main menu
+tooltip = "Insignien der European Defence Force";
+tooltipOwned = "Insignien der European Defence Force"; //Tooltip when mod icon is moused over in main menu
 
-name = "9. Jägerkompanie Insignien"; //Full name of the mod 
-overview = "All Emblems of the Bundeswehr and some specific to our clan."; //Description of the mod displayed in the "DLC" page of the main menu 
+name = "Insignien der European Defence Force"; //Full name of the mod 
+overview = "All Ranks of the Bundeswehr in European Weapon Color and some specific to our clan."; //Description of the mod displayed in the "DLC" page of the main menu 
 actionName = "GitHub";
-action = "https://github.com/Nilia119/9JgKp-Inisgnia"; //Link to take people to from "Website" button
-author = "[9.JgKp] Nilia";
+action = "https://github.com/Nilia119/EuDef-Inisgnia"; //Link to take people to from "Website" button
+author = "[EuDef] Nilia";
