0f5cea20241ba5533a454b4d77481dad703103c6
Nilia119 <niklas.rieck@gmail.com>
2023-11-25 15:26:53 +0100

    Removed Tom

--- Addons/9JgKp_Insignia_Player/config.cpp
+++ Addons/9JgKp_Insignia_Player/config.cpp
@@ -118,12 +118,6 @@ class CfgUnitInsignia {
 	  author="Nilia";
   };
 
- class 9Jgkp_Insignia_Tom: 9JgKp_Insignia_Logo
-    {
-      displayName = "9. JgKp Tom";
-      texture = "\9JgKp_Insignia_Player\Player\Bilder\Tom.paa";
-	  author="Nilia";
-  };
 
     class 9Jgkp_Insignia_CC: 9JgKp_Insignia_Logo
     {
